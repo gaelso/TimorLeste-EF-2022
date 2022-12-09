107171124f60d9ff5657f0b96c6edba6fc504705
gaelso-bkp <gaso.ella@gmail.com>
2022-12-09 17:59:22 +0700

    rm dup

--- R-activity-data/activity-data-samples.R
+++ R-activity-data/activity-data-samples.R
@@ -11,7 +11,7 @@ tt <- map_dfr(ad_files, function(x){
   url <- file.path("data/Activity data", x)
   
   tt <- read_csv(url, col_types = list(.default = "c")) %>%
-    mutate(surevyor_name = str_remove(x, "_.*"))
+    mutate(surveyor_name = str_remove(x, "_.*"))
   
 })
 
@@ -36,8 +36,51 @@ test2 <- tt %>%
 
 test2 %>% 
   select(
-    id, operator, surevyor_name, actively_saved, 
+    id, operator, surveyor_name, actively_saved, 
     actively_saved_on_year, actively_saved_on_month, actively_saved_on_day
     )
 
+tt2 <- tt %>%
+  mutate(
+    id = as.numeric(id),
+    survey_month = if_else(
+      as.numeric(actively_saved_on_month) < 10, 
+      paste0("0", actively_saved_on_month),
+      actively_saved_on_month),
+    survey_day = if_else(
+      as.numeric(actively_saved_on_day) < 10, 
+      paste0("0", actively_saved_on_day),
+      actively_saved_on_day),
+    survey_date = as.numeric(paste0(actively_saved_on_year, survey_month, survey_day)),
+    duplicate_records = if_else(id %in% vec, TRUE, FALSE)
+    ) %>%
+  filter(!is.na(id))
 
+table(tt2$duplicate_records)
+
+max_date <- tt2 %>%
+  group_by(id) %>%
+  summarise(max_date = max(survey_date))
+
+
+tt3 <- tt2 %>%
+  left_join(max_date, by = "id") %>%
+  filter(!(duplicate_records & survey_date != max_date)) %>%
+  select(-survey_month, -survey_day, -survey_date, -duplicate_records, -max_date) %>%
+  arrange(id)
+tt3
+
+test <- tt3 %>%
+  group_by(id) %>%
+  summarise(count = n()) 
+
+table(test$count)
+
+write_csv(tt3, paste0("results/sbae_2km_TL_clean_", Sys.Date(), ".csv"))
+
+## Compa with original grid
+sbae_original <- read_csv(list.files("data/Activity data/original grid", full.names = T))
+sbae_original
+
+sbae_id
+   
